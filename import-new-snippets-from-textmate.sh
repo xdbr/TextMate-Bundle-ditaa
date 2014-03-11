fe710bc0a7d94d389bb4b7a98a9f93f7d0f4fea1
Daniel Bruder <daniel@codeedition.de>
2014-03-11 21:30:27 +0100

    Release 0.1.0

--- import-new-snippets-from-textmate.sh
+++ import-new-snippets-from-textmate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set +x
 cd ..
-cp -r ~/Library/Application\ Support/TextMate/Bundles/ditaa.tmbundle/* ${OLDPWD}
+cp -r ~/Library/Application\ Support/TextMate/Bundles/ditaa.tmbundle/*          ${OLDPWD} || \
+cp -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/ditaa.tmbundle/* ${OLDPWD}
 cd -
